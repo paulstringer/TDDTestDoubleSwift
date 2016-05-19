20436928c2e3d0c01694536633f783f271e6d043
Paul Stringer <paulstringer@mac.com>
2016-05-19 12:49:48 +0100

    Ready to begin

---
+++ TestDoublesSwift/Sale.swift
@@ -0,0 +1,13 @@
+//
+//  Sale.swift
+//  TestDoublesSwift
+//
+//  Created by Paul Stringer on 19/05/2016.
+//  Copyright © 2016 stringerstheory. All rights reserved.
+//
+
+import Foundation
+
+class Sale {
+    
+}
